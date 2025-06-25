c42fac09237d61b5eefea1a0bc3dccaa1270383a
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-25 19:54:28 +0300

    Alışveriş sepeti tasarımı basitçe yapıldı ve ürün ekleme işlemi gerçekleştirildi.

--- StoreApp.Web/Pages/Cart.cshtml.cs
+++ StoreApp.Web/Pages/Cart.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StoreApp.Data.Abstract;
+using StoreApp.Web.Helpers;
+using StoreApp.Web.Models;
 
 namespace StoreApp.Web.Pages
 {
     public class CartModel : PageModel
     {
+        private IStoreRepository _storeRepository;
+
+        public CartModel(IStoreRepository storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
+
+        public Cart? Cart { get; set; }
         public void OnGet()
         {
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+
+        }
+
+        public IActionResult OnPost(int Id)
+        {
+            var product = _storeRepository.Products.FirstOrDefault(x => x.Id == Id);
+
+            if(product!= null)
+            {
+                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                Cart.AddItem(product, 1);
+                HttpContext.Session.SetJson("cart", Cart);
+            }
+
+            return RedirectToPage("/Cart");
         }
     }
 }
