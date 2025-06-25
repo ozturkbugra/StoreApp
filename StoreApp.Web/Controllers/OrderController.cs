03e78fbc7f56a47fc27f10c0746f200f9cda4c82
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-25 21:02:16 +0300

    Sipariş sayfası yapıldı.

---
+++ StoreApp.Web/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Web.Models;
+
+namespace StoreApp.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private Cart _cart;
+
+        public OrderController(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IActionResult CheckOut()
+        {
+            return View(new OrderModel()
+            {
+                Cart = _cart
+            });
+        }
+    }
+}
---
+++ StoreApp.Web/Models/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using StoreApp.Data.Concrete;
+
+namespace StoreApp.Web.Models
+{
+    public class OrderModel
+    {
+            public int Id { get; set; }
+
+            public DateTime OrderDate { get; set; }
+
+            public string Name { get; set; }
+            public string City { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+            public string AddressLine { get; set; }
+
+            public Cart Cart { get; set; } = new();
+        
+    }
+}
