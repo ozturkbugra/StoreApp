4c2913d03076a0c6f63ee360d71c02dec4943e1f
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-25 19:30:14 +0300

    Cart model eklendi

---
+++ StoreApp.Web/Models/Cart.cs
@@ -0,0 +1,46 @@
+﻿using StoreApp.Data.Concrete;
+
+namespace StoreApp.Web.Models
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            var item = Items.Where(x => x.Product.Id == product.Id).FirstOrDefault();
+
+            if (item == null)
+            {
+                Items.Add(new CartItem { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(Product product)
+        {
+            Items.RemoveAll(x => x.Product.Id == product.Id);
+        }
+
+        public decimal CalculateTotal()
+        {
+            return Items.Sum(x => x.Product.Price * x.Quantity);
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+    }
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+
+        public Product Product { get; set; } = new();
+
+        public int Quantity { get; set; }
+    }
+}
